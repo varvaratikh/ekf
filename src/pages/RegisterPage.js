6cd7159f2b70475685f83a8c11466207f2d09e94
varvaratikh <tikhonovavarvara181203@gmail.com>
2024-07-13 19:34:40 +0300

    added the correct version of RegisterPage

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Register from "../components/RedRegisterButton";
+import RegisterForm from "../components/RegisterForm";
+import RedRegisterButton from "../components/RedRegisterButton";
+
 
 const RegisterPage = () => {
     return (
-        <div className="flex flex-col items-center justify-center h-screen relative">
-            <h1 className="text-3xl font-semibold pt-40 text-center text-custom-black">Прогнозирование коммерческого <br/> предложения</h1>
-            <Register />
+        <div className="flex flex-col items-center justify-center min-h-screen pt-1">
+            <h1 className="text-3xl font-semibold text-center text-custom-black mb-12">Прогнозирование коммерческого <br/> предложения</h1>
+            <div className="w-full max-w-sm">
+                <RegisterForm/>
+                <div className="mt-24">
+                    <RedRegisterButton/>
+                </div>
+            </div>
             <p className="absolute bottom-0 right-0 mb-4 mr-2 text-custom-team-logo text-lg font-semibold">Hackoholics</p>
         </div>
     );
