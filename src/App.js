698cc90d82906fce0374c78a95c93b594f93d7fb
varvaratikh <tikhonovavarvara181203@gmail.com>
2024-07-13 13:06:24 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,30 +1,60 @@
-import React from 'react';
-import Header from './components/Header';
-import Login from './components/Login';
-import Register from './components/Register';
-import FileUpload from './components/FileUpload';
-import FileList from './components/FileList';
-import PredictionForm from './components/PredictionForm';
-import DownloadCSV from './components/DownloadCSV';
+// import React from 'react';
+// import Header from './components/Header';
+// import Login from './components/Login';
+// import Register from './components/Register';
+// import FileUpload from './components/FileUpload';
+// import FileList from './components/FileList';
+// import PredictionForm from './components/PredictionForm';
+// import DownloadCSV from './components/DownloadCSV';
+//
+// import ImageUploadPage from './pages/ImageUploadPage';
+//
+//
+// const App = () => {
+//     return (
+//         <div className="container mx-auto p-4 font-sans">
+//             {/*<Header />*/}
+//             <main>
+//                 {/*<Login />*/}
+//                 {/*<Register />*/}
+//                 {/*<FileUpload />*/}
+//                 {/*<FileList />*/}
+//                 {/*<PredictionForm />*/}
+//                 {/*<DownloadCSV />*/}
+//                 <ImageUploadPage />
+//             </main>
+//         </div>
+//     );
+// };
+//
+// export default App;
 
-import ImageUploadPage from './pages/ImageUploadPage';
+import React, { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
 
+export default function App() {
+    const onDrop = useCallback((acceptedFiles) => {
+        alert(acceptedFiles[0]?.name || 'No files selected');
+        console.log("Now you can do anything with this file as per your requirement");
+    }, []);
 
-const App = () => {
+    const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
     return (
-        <div className="container mx-auto p-4 font-sans">
-            {/*<Header />*/}
-            <main>
-                {/*<Login />*/}
-                {/*<Register />*/}
-                {/*<FileUpload />*/}
-                {/*<FileList />*/}
-                {/*<PredictionForm />*/}
-                {/*<DownloadCSV />*/}
-                <ImageUploadPage />
-            </main>
+        <div style={{ display: 'block', width: 700, padding: 30 }}>
+            <h4>React-Dropzone Module Demo</h4>
+            <div
+                {...getRootProps()}
+                style={{
+                    border: '2px dashed #007bff',
+                    padding: '20px',
+                    textAlign: 'center',
+                    cursor: 'pointer',
+                }}
+            >
+                <input {...getInputProps()} />
+                <p>Click to select file or drag-and-drop the file here!!</p>
+            </div>
         </div>
     );
-};
-
-export default App;
+}
