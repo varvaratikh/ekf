57b47c8a3d2731ee5f78cd6ec1305f5c50245b86
varvaratikh <tikhonovavarvara181203@gmail.com>
2024-07-14 21:14:56 +0300

    added changes

--- src/pages/ImageUploadPage.js
+++ src/pages/ImageUploadPage.js
@@ -1,14 +1,51 @@
-import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import DownloadCSV from "../components/DownloadCSV";
+import React, { useCallback, useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useDropzone } from 'react-dropzone';
+import axios from 'axios';
 import SideModalComponent from '../components/SideModalComponent';
 import burger from '../images/Burger.png';
 
-const ImageEditingPage = () => {
-    const location = useLocation();
-    const { image, userId, response } = location.state || {};
+const ImageUploadPage = () => {
+    const [uploadedFile, setUploadedFile] = useState(null);
+    const [base64File, setBase64File] = useState('');
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [images, setImages] = useState(response?.images || []);
+    const [userId, setUserId] = useState(null);
+    const [images, setImages] = useState([]);
+    const navigate = useNavigate();
+
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.state && location.state.response) {
+            const { userId, images } = location.state.response;
+            setUserId(userId);
+            setImages(images);
+        }
+    }, [location.state]);
+
+    const onDrop = useCallback((acceptedFiles) => {
+        if (acceptedFiles.length > 1) {
+            alert('Please select only 1 file.');
+            return;
+        }
+
+        const file = acceptedFiles[0];
+        setUploadedFile(file);
+
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            setBase64File(reader.result);
+        };
+
+        reader.readAsDataURL(file);
+    }, []);
+
+    const removeImage = () => {
+        setUploadedFile(null);
+        setBase64File('');
+    };
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop, accept: 'image/*' });
 
     const openModal = () => {
         setIsModalOpen(true);
@@ -16,6 +53,31 @@ const ImageEditingPage = () => {
 
     const closeModal = () => {
         setIsModalOpen(false);
+    };
+
+    const handleSend = async () => {
+        console.log('Sending image...');
+        console.log('Base64 File:', base64File);
+
+        if (!base64File) {
+            alert('Please upload a file first.');
+            return;
+        }
+
+        try {
+            const response = await axios.post('http://5a02-57-129-1-195.ngrok-free.app/image', {
+                image: base64File
+            });
+
+            if (response.status === 200) {
+                console.log('Response:', response.data);
+                navigate('/editing', { state: { image: base64File, userId, response: response.data } });
+            } else {
+                console.error('Request failed:', response);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
     };
 
     return (
@@ -28,26 +90,38 @@ const ImageEditingPage = () => {
                     onClick={openModal}
                 />
             </div>
-            {image ? (
+            {uploadedFile ? (
                 <div className="w-4/6 flex flex-col items-center justify-center mt-8 mb-10">
-                    <img src={image} alt="Uploaded file" className="max-h-[70vh] max-w-full object-contain" />
+                    <div className="relative">
+                        <button
+                            className="absolute top-0 right-0 mt-2 mr-2 p-1"
+                            onClick={removeImage}
+                        >
+                            <svg className="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                        <img src={URL.createObjectURL(uploadedFile)} alt="Uploaded file" className="max-h-[70vh] max-w-full object-contain" />
+                    </div>
                 </div>
             ) : (
-                <div className="flex flex-col items-center mt-8 mb-10">
-                    <p>No image uploaded</p>
-                    <DownloadCSV />
+                <div {...getRootProps({ className: 'dropzone' })} className={`w-4/6 h-1/2 border-3 border-dashed 
+          ${isDragActive ? 'border-custom-red' : 'border-custom-blue'} flex items-center justify-center mt-8 mb-10 
+          border-custom-blue hover:border-custom-red hover:cursor-pointer`}>
+                    <input {...getInputProps()} />
+                    {
+                        isDragActive ?
+                            <p>Drop the file here...</p> :
+                            <p>Select a file</p>
+                    }
                 </div>
             )}
-            {userId && response && (
-                <div>
-                    <p>User ID: {userId}</p>
-                    <pre>{JSON.stringify(response, null, 2)}</pre>
-                </div>
-            )}
-            {image && <DownloadCSV />}
+
+            <button onClick={handleSend} className="bg-custom-grey py-2 px-40 cursor-pointer mb-10">Отправить</button>
+
             <SideModalComponent isOpen={isModalOpen} onRequestClose={closeModal} images={images} />
         </div>
     );
 };
 
-export default ImageEditingPage;
+export default ImageUploadPage;
