63bf0a4a1cb2e61746babb12a03027225158a5f9
varvaratikh <tikhonovavarvara181203@gmail.com>
2024-07-16 18:30:27 +0300

    fixed right window

--- src/components/DownloadCSV.js
+++ src/components/DownloadCSV.js
@@ -11,3 +11,72 @@ const DownloadCSV = () => {
 };
 
 export default DownloadCSV;
+
+// import React from 'react';
+// import axios from 'axios';
+//
+// const DownloadCSV = () => {
+//     const handleDownload = async () => {
+//         const url = `https://193.124.33.166:8094/users/csv`;
+//         const data = { info: {} };
+//
+//         console.log('Отправляем запрос на:', url);
+//         console.log('С данными:', data);
+//
+//         try {
+//             const response = await axios.post(url, data, {
+//                 responseType: 'blob',
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                     'Access-Control-Allow-Origin': '*'
+//                 }
+//             });
+//
+//             if (response.status === 200) {
+//                 const downloadUrl = window.URL.createObjectURL(new Blob([response.data]));
+//                 const link = document.createElement('a');
+//                 link.href = downloadUrl;
+//                 link.setAttribute('download', 'users.csv');
+//                 document.body.appendChild(link);
+//                 link.click();
+//                 document.body.removeChild(link);
+//             } else {
+//                 console.error('Ошибка сервера:', response.statusText);
+//             }
+//         } catch (error) {
+//             if (error.response) {
+//                 const reader = new FileReader();
+//                 reader.onload = () => {
+//                     const errorText = reader.result;
+//                     console.error('Сообщение об ошибке от сервера:', errorText);
+//                 };
+//                 reader.readAsText(error.response.data);
+//
+//                 console.error('Код статуса:', error.response.status);
+//                 console.error('Заголовки:', error.response.headers);
+//             } else if (error.request) {
+//                 console.error('Запрос был сделан, но ответа не получено', error.request);
+//             } else {
+//                 console.error('Ошибка:', error.message);
+//             }
+//             console.error('Конфигурация запроса:', error.config);
+//         }
+//     };
+//
+//
+//     return (
+//         <div className="text-center mt-8">
+//             <button
+//                 type="button"
+//                 className="py-2 px-16 bg-custom-red text-custom-wight font-bold"
+//                 onClick={handleDownload}
+//             >
+//                 Скачать .csv
+//             </button>
+//         </div>
+//     );
+// };
+//
+// export default DownloadCSV;
+//
+
--- src/components/SideModalComponent.js
+++ src/components/SideModalComponent.js
@@ -3,12 +3,13 @@ import Element from '../components/Element';
 
 const SideModalComponent = ({ isOpen, onRequestClose, mp, selectedBox, onBoxUpdate, onBoxDelete, onAddElement }) => {
     const items = mp ? Object.entries(mp).map(([key, value], index) => ({
-        color: 'red', // Set default color to red
+        color: 'red',
         text: `${key}: ${value}`
     })) : [];
 
     return (
-        <div className={`fixed top-10 right-0 bg-white shadow-lg transition-transform duration-300 ${isOpen ? 'translate-x-0' : 'translate-x-full'}`} style={{ width: '278px', height: '700px', margin: '10px 20px', overflow: 'hidden' }}>
+        <div className={`fixed top-10 right-0 bg-white shadow-lg transition-transform duration-300 
+      ${isOpen ? 'translate-x-0' : 'translate-x-full'}`} style={{ width: '278px', height: '700px', margin: `10px ${isOpen ? '20px' : '0'}`, overflowY: 'auto' }}>
             <div className="flex flex-col h-full p-8">
                 <div className="flex justify-start">
                     <button onClick={onRequestClose} className="p-1 focus:outline-none relative" style={{ top: '-15px', left: '-15px' }}>
